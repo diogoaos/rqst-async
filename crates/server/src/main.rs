f7d4a8107adc8904341c570cb5b1709d92834518
Diogo Anderson <diogoaos@gmail.com>
2024-12-20 11:37:20 -0300

    Added endpoint /chat. Closes #1

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,10 +1,34 @@
-use miniserve::{Content, Request, Response};
+use miniserve::{http::StatusCode, Content, Request, Response};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct Messages {
+    messages: Vec<String>,
+}
 
 fn index(_req: Request) -> Response {
     let content = include_str!("../index.html").to_string();
     Ok(Content::Html(content))
 }
 
+fn chat(_req: Request) -> Response {
+    match _req {
+        Request::Get => Err(StatusCode::METHOD_NOT_ALLOWED),
+        Request::Post(body) => {
+            let content = body;
+            let mut request_dto: Messages = serde_json::from_str(&content).unwrap();
+            let mut last_word = request_dto.messages.last().unwrap().clone();
+
+            last_word.push_str(" -> well said");
+            request_dto.messages.push(last_word);
+            Ok(Content::Json(serde_json::to_string(&request_dto).unwrap()))
+        }
+    }
+}
+
 fn main() {
-    miniserve::Server::new().route("/", index).run()
+    miniserve::Server::new()
+        .route("/", index)
+        .route("/chat", chat)
+        .run()
 }
